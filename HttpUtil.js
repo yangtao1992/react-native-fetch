cf5ef54d85e1152623fffa8d23acb79d945f1936
ouyangt <ouyangt@aixiangdao.com>
2019-05-21 13:56:02 +0800

    抽离了公共部分，新增连接超时

--- HttpUtil.js
+++ HttpUtil.js
@@ -1,73 +1,79 @@
 import SignUtils from "../util/SignUtils";
 let instance = null;
-let token = null;
+let requestGet = {
+    method: 'GET',
+};
+let requestPost = {
+    method: 'POST',
+    headers:{
+        'Accept':'application/json',
+        'Content-Type':'application/json',
+        'userToken': ''
+    },
+    body:'',
+};
+let timeout;
 
 export default class HttpUtil{
 
     static getInstance(){
-		
         if(!instance){
-			//可以在这做token值获取工作
-			token = "123";
             instance = new HttpUtil();
         }
         return instance;
     }
 
-    
-    //GET请求
+    // GET请求
     get(url,params){
-        if(params){
-			//url地址参数拼接
-            url = SignUtils.getSplicingUrl(url,params);
-        }
-        return new Promise(function(resolve, reject){
-            fetch(url, {
-                method: 'GET',
+        url = params ? SignUtils.getSplicingUrl(url,params) : url;
+        console.log("url ========请求地址==================>"+url);
+        return this.connect(url,requestGet);
+    }
+
+    //POST请求
+    post(url,bodyData,token){
+        console.log("url ====>"+url+"，body ====>"+JSON.stringify(bodyData)+"，token ====>"+token);
+        requestPost['headers']['userToken'] = token;
+        requestPost.body = JSON.stringify(bodyData);
+        return this.connect(url,requestPost);
+    }
+
+    requestData(url,requestWay){
+        return new Promise(function(resolve,reject){
+            fetch(url,requestWay).then((response)=>{
+                resolve(response);
+            }).catch((err)=>{
+                reject(err);
             })
-                .then((response)=>{
+        });
+    }
+
+    timeOut(){
+        return new Promise(function(resolve,reject){
+            timeout = setTimeout(function(){
+                reject('请求超时');
+            }, 30000);
+        });
+    }
+
+    connect(url,requestWay){
+        return new Promise(function(resolve,reject){
+            Promise
+                .race([instance.requestData(url,requestWay),instance.timeOut()])
+                .then(function(response){
+                    if(timeout)clearTimeout(timeout);
                     if(response.ok){
-                        return response.json();
-                    } else {
+                        resolve(response.json());
+                    }else{
+                        console.log("========请求失败==================>"+JSON.stringify(response.json));
                         reject(response.json())
                     }
                 })
-                .then((response)=>{
-                    resolve(response);
-                })
-                .catch((err)=>{
-                    reject({status:-1});
-                })
-        })
-    }
-
-
-    //POST请求
-    post(url,bodyData){
-        return new Promise(function(resolve, reject){
-            fetch(url, {
-                method: 'POST',
-                headers:{
-                    'Accept':'application/json',
-                    'Content-Type':'application/json',
-                    'userToken': token
-                    // 'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                body:JSON.stringify(bodyData),
-            })
-        .then((response)=>{
-                if(response.ok){
-                    return response.json();
-                } else {
-                    reject(response.json())
-                }
-            })
-        .then((response)=>{
-                resolve(response);
-            })
-        .catch((err)=>{
-                reject({status:-1});
-            })
-        })
+                .catch(function(err){
+                    console.log("========请求异常==================>"+err);
+                    if(timeout)clearTimeout(timeout);
+                    reject(err);
+                });
+        });
     }
 }
